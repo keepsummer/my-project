0e159ee3a4c5d295a9f9f79e0a26c2c0d55df37d
李志敏 <915377975@qq.COM>
2020-08-27 09:58:00 +0800

    feat：设置导航栏默认展开

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,14 +2,16 @@
     <el-container style="height: 500px; border: 1px solid #eee">
       <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
 
-        <el-menu router>
+        <el-menu router :default-openeds="['0','1']">
+<!--          :default-openeds="['0','1']" 默认展开导航栏,index 为0,1-->
 <!--          $router 是取index.js里面的router 固定语法-->
           <el-submenu  v-for="(item,index) in $router.options.routes" :index="index+''" :key="index">
             <template slot="title" ><i class="el-icon-message"></i>{{item.name}}</template>
-            <el-menu-item-group v-for="(item1,index2) in item.children":key="index2">
+            <el-menu-item-group v-for="(item1,index2) in item.children" :key="index2">
 <!--              :key 是解决vue不报warnning的-->
 <!--              index标签是用来路由到页面的,如果点击页面1 就跳转到page1-->
-              <el-menu-item :index="item1.path">{{item1.name}}</el-menu-item>
+              <el-menu-item :index="item1.path" :class="$route.path == item1.path?'is-active':''" >{{item1.name}}</el-menu-item>
+<!--              :class="$route.path == item1.path?'is-active':''" 选中导航栏中的页面，使其变成选中状态-->
             </el-menu-item-group>
           </el-submenu>
         </el-menu>
